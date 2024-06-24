f96601bdb87f4bdbc53e78a8436a564cf52d74c4
João Inácio <j.g.c.inacio@fys.uio.no>
2024-06-24 09:38:14 +0200

    sample eq and spec in all MCS

--- src/main.c
+++ src/main.c
@@ -149,17 +149,17 @@ int main(int argc, char **argv)
           monte_carlo_step(b, beta, pcg32_double_r(&rng), &flux_conf, &new_flux_conf);
         }
 
-        if (n_scal > 0) {
+        if (n_scal > 0 || n_spec > 0 || n_eq > 0) {
           sample(obs_scal, n_scal, obs_eq, n_eq, obs_spec, n_spec, beta, &flux_conf);
         }
-        if ((n_spec > 0 || n_eq > 0) && (t == sim.mc_sweeps - 1 || t == sim.mc_sweeps / 2)) {
-          sample_obs_eq(obs_eq, n_eq, beta, &flux_conf);
-          sample_obs_spec(obs_spec, n_spec, beta, &flux_conf);
-        }
+        // if ((n_spec > 0 || n_eq > 0) && (t == sim.mc_sweeps - 1 || t == sim.mc_sweeps / 2)) {
+        //   sample_obs_eq(obs_eq, n_eq, beta, &flux_conf);
+        //   sample_obs_spec(obs_spec, n_spec, beta, &flux_conf);
+        // }
       }
 
       // Write bin to file
-      if (n_scal > 0 || n_eq > 0) {
+      if (n_scal > 0 || n_spec > 0 || n_eq > 0) {
         write_observables(obs_scal, n_scal, obs_eq, n_eq, obs_spec, n_spec);
       }
       write_configuration(&flux_conf);
@@ -192,13 +192,13 @@ int main(int argc, char **argv)
             monte_carlo_step(b, replicas[t_id].beta, pcg32_double_r(&rng), replicas[t_id].flux, &new_flux_conf);
           }
 
-          if (t_id == n_replica_walkers - 1 && n_scal > 0) {
+          if (t_id == n_replica_walkers - 1 && (n_scal > 0 || n_spec > 0 || n_eq > 0)) {
             sample(obs_scal, n_scal, obs_eq, n_eq, obs_spec, n_spec, replicas[t_id].beta, replicas[t_id].flux);
           }
-          if (t_id == n_replica_walkers - 1 && (n_spec > 0 || n_eq > 0) && (t == sim.mc_sweeps - 1 || t == sim.mc_sweeps / 2)) {
-            sample_obs_eq(obs_eq, n_eq, replicas[t_id].beta, replicas[t_id].flux);
-            sample_obs_spec(obs_spec, n_spec, replicas[t_id].beta, replicas[t_id].flux);
-          }
+          // if (t_id == n_replica_walkers - 1 && (n_spec > 0 || n_eq > 0) && (t == sim.mc_sweeps - 1 || t == sim.mc_sweeps / 2)) {
+          //   sample_obs_eq(obs_eq, n_eq, replicas[t_id].beta, replicas[t_id].flux);
+          //   sample_obs_spec(obs_spec, n_spec, replicas[t_id].beta, replicas[t_id].flux);
+          // }
 
           // Exchange configurations
           if (t % n_exchange_frequency == 0) {
@@ -216,7 +216,7 @@ int main(int argc, char **argv)
         }
 
         // Write bin to file
-        if (t_id == n_replica_walkers - 1 && (n_scal > 0 || n_eq > 0)) {
+        if (t_id == n_replica_walkers - 1 && (n_scal > 0 || n_spec > 0 || n_eq > 0)) {
           write_observables(obs_scal, n_scal, obs_eq, n_eq, obs_spec, n_spec);
         }
         if (t_id == n_replica_walkers - 1) {
