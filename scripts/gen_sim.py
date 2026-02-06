39238c7b0974f70b69d710912108bbae9cafbbce
João Inácio <joao.carvalho-inacio@uni-wuerzburg.de>
2026-02-06 11:39:27 +0100

    fix gen_sim

--- scripts/gen_sim.py
+++ scripts/gen_sim.py
@@ -6,7 +6,7 @@ def frange(start, stop, step):
     start += step
 
 
-N = 182 # 182 364 448
+L = 6
 alpha = 1.0
 
 T_vals = [round(j, 2) for j in list(frange(0.01, 2.05, 0.05))]
@@ -23,11 +23,11 @@ with open(output_file, "w") as f:
       run_str = "Y"
       n_sim += 1
 
-    line = f"{run_str}  {N}  {alpha}  {round(1.0/T, 2)}   50  10 \n"
+    line = f"{run_str}  {L}  {L}  {alpha}  {round(1.0/T, 2)}   50  10 \n"
     f.write(line)
 
   f.write("stop\n")
-  f.write("Run N_R alpha_R  Beta_R  NSW_R NB_R\n")
-  f.write("Name=(Run N_R alpha_R  Beta_R  NSW_R NB_R)\n")
+  f.write("Run Lx_R Ly_R alpha_R  Beta_R  NSW_R NB_R\n")
+  f.write("Name=(Run Lx_R Ly_R alpha_R  Beta_R  NSW_R NB_R)\n")
 
 print(f"#simulations: {n_sim}")
